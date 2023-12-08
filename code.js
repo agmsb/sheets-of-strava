991ec19f6dd536377dd5b94e3872abee44728c1e
anthony bushong <agmsb@users.noreply.github.com>
2023-12-07 18:56:13 -0800

    Update code.js

--- code.js
+++ code.js
@@ -7,51 +7,6 @@ function onOpen() {
     .addToUi();
 }
 
-function initiateStrava(context) {
-  
-  var service = getStravaService();
-  if (service.hasAccess()) {
-    Logger.log('App has access.');
-    if (context == 'rides') {
-      var api = 'https://www.strava.com/api/v3/athlete/activities';
-      var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-      var sheet = spreadsheet.getSheetByName('epoch');
-      var epoch = sheet.getRange('A1').getValue() + 60;
-      var query = '?after=' + epoch + '&per_page=200';
-      var headers = {
-        Authorization: 'Bearer ' + service.getAccessToken()
-      };
-      var options = {
-        headers: headers,
-        method : 'GET',
-        muteHttpExceptions: true
-      };
-    } 
-    if (context == 'segments') {
-      var segmentID = '141491'
-      var api = 'https://www.strava.com/api/v3/segment_efforts';
-      var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-      var query = '?segment_id=' + segmentID + '&per_page=200';
-      var headers = {
-        Authorization: 'Bearer ' + service.getAccessToken()
-      };
-      var options = {
-        headers: headers,
-        method : 'GET',
-        muteHttpExceptions: true
-      };
-    }
-    var response = JSON.parse(UrlFetchApp.fetch(api + query, options));
-    return response;  
-  }
-  else {
-    Logger.log("App has no access yet.");
-    var authorizationUrl = service.getAuthorizationUrl();
-    Logger.log("Open the following URL and re-run the script: %s",
-        authorizationUrl);
-  }
-}
-
 function getRides() {
   
   var context = 'rides'
@@ -107,5 +62,50 @@ function getSegmentEfforts() {
   );
   if(segmentEffortData.length > 0){
     sheet.getRange(sheet.getLastRow() + 1, 1, segmentEffortData.length, segmentEffortData[0].length).setValues(segmentEffortData);
+  }
+}
+
+function initiateStrava(context) {
+  
+  var service = getStravaService();
+  if (service.hasAccess()) {
+    Logger.log('App has access.');
+    if (context == 'rides') {
+      var api = 'https://www.strava.com/api/v3/athlete/activities';
+      var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+      var sheet = spreadsheet.getSheetByName('epoch');
+      var epoch = sheet.getRange('A1').getValue() + 60;
+      var query = '?after=' + epoch + '&per_page=200';
+      var headers = {
+        Authorization: 'Bearer ' + service.getAccessToken()
+      };
+      var options = {
+        headers: headers,
+        method : 'GET',
+        muteHttpExceptions: true
+      };
+    } 
+    if (context == 'segments') {
+      var segmentID = '141491'
+      var api = 'https://www.strava.com/api/v3/segment_efforts';
+      var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+      var query = '?segment_id=' + segmentID + '&per_page=200';
+      var headers = {
+        Authorization: 'Bearer ' + service.getAccessToken()
+      };
+      var options = {
+        headers: headers,
+        method : 'GET',
+        muteHttpExceptions: true
+      };
+    }
+    var response = JSON.parse(UrlFetchApp.fetch(api + query, options));
+    return response;  
+  }
+  else {
+    Logger.log("App has no access yet.");
+    var authorizationUrl = service.getAuthorizationUrl();
+    Logger.log("Open the following URL and re-run the script: %s",
+        authorizationUrl);
   }
 }
